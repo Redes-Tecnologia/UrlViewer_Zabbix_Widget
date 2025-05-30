d667999c3061e7e392ae71051c2b6eb38b4c4dfc
matiashteixeira <mateus.teixeira1@estudante.ufla.br>
2025-05-30 08:32:11 -0300

    Alteração do nome

--- includes/WidgetForm.php
+++ includes/WidgetForm.php
@@ -18,7 +18,7 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
-namespace Modules\OpenAI\Includes;
+namespace Modules\URLViewerTeste\Includes;
 
 use Zabbix\Widgets\CWidgetField;
 use Zabbix\Widgets\CWidgetForm;
